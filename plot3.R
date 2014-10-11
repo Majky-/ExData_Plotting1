6c602ff803f800b516cfe792841d26e3658ee295
Majky- <majky@skaut.cz>
2014-10-11 17:04:38 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,6 @@
+plot(gr_d$dt, gr_d$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+lines(gr_d$dt, gr_d$Sub_metering_1, col="black")
+lines(gr_d$dt, gr_d$Sub_metering_2, col="red")
+lines(gr_d$dt, gr_d$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+dev.print(png, file="plot3.png", width=480, height=480)
